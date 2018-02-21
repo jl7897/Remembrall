eda393113eb79220e5a5add73a21932103d8d241
Jake Lee <jakelee@Jakes-MacBook-Pro.local>
2018-02-21 12:16:21 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 import reducers from './reducers';
 import './style.scss';
 
@@ -12,8 +13,14 @@ const App = () => (
 );
 
 
-ReactDOM.render((
-  <Provider store={reducers} >
-    <App />
+ReactDOM.render(
+  (
+  <Provider store={createStoreWithMiddleware(reducers)} >
+    <BrowserRouter>
+      <div>
+        <Route path='/' component={App} />
+      </div>
+  </BrowserRouter>
   </Provider>
-), document.getElementById('app'));
+  ), document.getElementById('app')
+);
