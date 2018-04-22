451d6873deca91b8c476a459b1c971692377c7f4
Jake Lee <jacob.lee715@gmail.com>
2018-04-21 20:30:30 -0500

    spotify oauth, playlist, and playlist track retrieval working

--- server/index.js
+++ server/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const db = require('../database');
+// const db = require('../database');
 
 const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '/../public')));
-
 
 app.listen(port, () => {
   console.log(`listening on port ${port}!`);
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 'React Spotify',
-      authToken: '',
-      authorized: false,
-      profile: [],
+      play_lists: [],
+      auth: {},
     };
+  }
+
+  componentWillMount() {
+    this.setState({ auth: this.props.location.state });
+    axios.get(`https://api.spotify.com/v1/me/playlists?access_token=${this.props.location.state.token}`)
+      .then((response) => {
+        console.log(response.data);
+        this.setState({ play_lists: response.data.items });
+      })
+      .then(() => {
+        axios.get(`${this.state.play_lists[2].tracks.href}?access_token=${this.props.location.state.token}`)
+          .then(data => console.log('tracks: ', data))
+          .catch(error => console.log(error));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   render() {
     return (
-      <div>Logged in successfully, {this.props.location.state.user.email}!</div>
+      <div>
+      {this.state.auth.token ? null : <Redirect to='/'/>}
+      <div>Logged in successfully, {this.state.auth.user.email}!</div>
+      {this.state.play_lists.map(item => <div key={item.name}>{item.name}</div>)}
+      </div>
     );
   }
 }
